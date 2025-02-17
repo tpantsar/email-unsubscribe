640d9d4c1236df44abe6f4603868072feaa8b7cc
tpantsar <tomi.pantsar@gmail.com>
2025-02-17 10:47:28 +0200

    add readme, requirements.txt, gitignore

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Python-Email-Auto-Unsubscribe
+
+## .env file
+
+```shell
+EMAIL=""
+PASSWORD=""
+```
+
+## Virtual environment, dependencies and running the script
+
+```shell
+python -m venv .venv
+source .venv/Scripts/activate # Windows / MacOS
+source .venv/bin/activate # Linux
+
+pip install -r requirements.txt
+python main.py
+```
--- main.py
+++ main.py
@@ -1,14 +1,16 @@
-from dotenv import load_dotenv
-import imaplib
 import email
+import imaplib
 import os
-from bs4 import BeautifulSoup
+
 import requests
+from bs4 import BeautifulSoup
+from dotenv import load_dotenv
 
 load_dotenv()
 
 username = os.getenv("EMAIL")
 password = os.getenv("PASSWORD")
+
 
 def connect_to_mail():
     mail = imaplib.IMAP4_SSL("imap.gmail.com")
@@ -16,10 +18,16 @@ def connect_to_mail():
     mail.select("inbox")
     return mail
 
+
 def extract_links_from_html(html_content):
     soup = BeautifulSoup(html_content, "html.parser")
-    links = [link["href"] for link in soup.find_all("a", href=True) if "unsubscribe" in link["href"].lower()]
+    links = [
+        link["href"]
+        for link in soup.find_all("a", href=True)
+        if "unsubscribe" in link["href"].lower()
+    ]
     return links
+
 
 def click_link(link):
     try:
@@ -31,17 +39,18 @@ def click_link(link):
     except Exception as e:
         print("Error with", link, str(e))
 
+
 def search_for_email():
     mail = connect_to_mail()
     _, search_data = mail.search(None, '(BODY "unsubscribe")')
     data = search_data[0].split()
-    
+
     links = []
-    
+
     for num in data:
         _, data = mail.fetch(num, "(RFC822)")
         msg = email.message_from_bytes(data[0][1])
-        
+
         if msg.is_multipart():
             for part in msg.walk():
                 if part.get_content_type() == "text/html":
@@ -50,19 +59,21 @@ def search_for_email():
         else:
             content_type = msg.get_content_type()
             content = msg.get_payload(decode=True).decode()
-            
+
             if content_type == "text/html":
                 links.extend(extract_links_from_html(content))
-    
+
     mail.logout()
     return links
-    
+
+
 def save_links(links):
     with open("links.txt", "w") as f:
-        f.write("\n".join(links)) 
-    
+        f.write("\n".join(links))
+
+
 links = search_for_email()
 for link in links:
     click_link(link)
-    
+
 save_links(links)
